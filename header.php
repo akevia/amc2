7fd36f7e4fb66c6cf6ff02c718f8307039d7bdaf
Jorge Quezada <jorgequezada@akevia.mx>
2019-07-16 12:15:33 -0500

    Inicio de tema

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php the_title(); ?></title>
+</head>
+<body>
+
+<header>
+<a href=""><img src="" alt=""></a>
+<nav></nav>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</nav>
+</header>
