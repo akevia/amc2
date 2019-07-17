b8c81953eb66dbe4a4f0a51de711abd12a8ac1e3
Jorge Quezada <Akevia09>
2019-07-17 14:40:18 -0500

    Diseño de "contacto" y "que podemos hacer ..."

--- header.php
+++ header.php
@@ -1,32 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php the_title(); ?></title>
-    <?php wp_head();?>
+    <?php wp_head(); ?>
 </head>
+
 <body>
 
-<header>
-<a href="#" class="brand">
-<img src="<?php echo get_template_directory_uri().'/img/logo-amc-white.png'?>" alt="Logo AMC 2 White" class="logo-header">
-</a>
+    <header>
+        <a href="#" class="brand">
+            <img src="<?php echo get_template_directory_uri() . '/img/logo-amc-blue.png' ?>" alt="Logo AMC 2 White" class="logo-header">
+        </a>
 
-<nav class="nav-menu">
-    <ul>
-        <li>Nosotros </li>
-        <li>¿Qué podemos hacer por ti?</li>
-        <li>Competencias</li>
-        <li>Programas</li>
-        <li>Blog</li>
-        <li>Testimoniales</li>
-        <li>Contacto</li>
-    </ul>
-</nav>
+        <nav class="nav-menu">
+            <ul>
+                <li>Nosotros </li>
+                <li>¿Qué podemos hacer por ti?</li>
+                <li>Competencias</li>
+                <li>Programas</li>
+                <li>Blog</li>
+                <li>Testimoniales</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
 
-<div class="search-container">
-    &nbsp
-</div>
-</header>
+        <div class="search-container">
+            &nbsp
+        </div>
+    </header>
--- single-page.php
+++ single-page.php
@@ -6,23 +6,88 @@
 
 get_header();
 ?>
-<div class="row-container flexbox vertical-middle">
+<!-- <div class="row-container flexbox vertical-middle">
     <h1>
         Capacitando a la industria <br> en el desarrollo de competencias
     </h1>
+</div> -->
+
+<div class="row-container flexbox vertical-middle">
+    <div class="content">
+        <div>
+            <span class="small-title">
+                ¿Qué podemos hacer por ti?
+            </span>
+        </div>
+
+        <div class="grid">
+            <div class="colmn">
+                <img src="<?php echo get_template_directory_uri() . '/img/file-grey.png' ?>" class="column-img">
+                <img src="<?php echo get_template_directory_uri() . '/img/file-white.png' ?>" class="column-img-hover">
+                <div>
+                    <div class="column-title">
+                        Plan de estudios completo
+                    </div>
+
+
+                    <div class="column-desc">
+                        <p>
+                            Plan de estudios completo, que incluye manufactura lean y herramientas digitales avanzadas, como mantenimiento predictivo y análisis de rendimientos.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="colmn">
+                <img src="<?php echo get_template_directory_uri() . '/img/people-grey.png' ?>" class="column-img">
+                <img src="<?php echo get_template_directory_uri() . '/img/people-white.png' ?>" class="column-img-hover">
+                <div>
+                    <div class="column-title">
+                        Plan de estudios completo
+                    </div>
+                </div>
+            </div>
+            <div class="colmn">
+                <img src="<?php echo get_template_directory_uri() . '/img/mind-grey.png' ?>" class="column-img">
+                <img src="<?php echo get_template_directory_uri() . '/img/mind-white.png' ?>" class="column-img-hover">
+                <div>
+                    <div class="column-title">
+                        Plan de estudios completo
+                    </div>
+                </div>
+            </div>
+            <div class="colmn">
+                <img src="<?php echo get_template_directory_uri() . '/img/man-grey.png' ?>" class="column-img">
+                <img src="<?php echo get_template_directory_uri() . '/img/man-white.png' ?>" class="column-img-hover">
+                <div>
+                    <div class="column-title">
+                        Plan de estudios completo
+                    </div>
+                </div>
+            </div>
+            <div class="colmn">
+                <img src="<?php echo get_template_directory_uri() . '/img/book-grey.png' ?>" class="column-img">
+                <img src="<?php echo get_template_directory_uri() . '/img/book-white.png' ?>" class="column-img-hover">
+                <div>
+                    <div class="column-title">
+                        Plan de estudios completo
+                    </div>
+
+                    <p>
+                        Plan de estudios completo, que incluye manufactura lean y herramientas digitales avanzadas, como mantenimiento predictivo y análisis de rendimientos.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-
-<div class="row-container">
-
-</div>
-
+<!-- 
 <div class="row-container flexbox" style="background-image: url(<?php echo get_template_directory_uri() . '/img/contact-bg.jpg'; ?>); background-position: bottom right;">
     <div class="colmn flexbox vertical-middle">
         <div class="content">
             <div>
-                <span class="title">Contactanos</span>
+                <span class="title">Contáctanos</span>
             </div><br>
-            <p>¿Quieres diseñemos un plan de formación para tu empresa?</p>
+            <p class="desc color-pink">¿Quieres que diseñemos un plan <br /> de formación para tu empresa?</p>
         </div>
     </div>
     <div class="colmn flexbox vertical-middle horizontal-center">
@@ -36,11 +101,11 @@ get_header();
                     <input type="text" name="" id="" class="i-50">
                 </div>
                 <div class="form-row">
-                    <input type="text" name="" id="" class="i-50">
+                    <input type="text" name="" id="" class="i-50" placeholder="Area(Compras, Etc)">
                     <input type="text" name="" id="" class="i-50">
                 </div>
                 <div class="form-row">
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                    <textarea name="" id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
                 </div>
                 <div class="form-row">
                     <input type="submit" value="Enviar">
@@ -48,6 +113,6 @@ get_header();
             </form>
         </div>
     </div>
-</div>
+</div> -->
 <?php
 get_footer();
