384bbd5f5d98d6ff7748b05a2ceac7ecab258f6b
Jorge Quezada <Akevia09>
2019-07-19 16:20:34 -0500

    Inicio de Blog

--- single-page.php
+++ single-page.php
@@ -369,7 +369,51 @@ get_header();
 
 </div><!-- Programas -->
 
+<!-- Blog -->
+<div class="row-container flexbox vertical-middle">
+    <div class="content">
+        <div class="flexbox vertical-middle horizontal-space">
+            <div class="small-title">Blog</div>
 
+            <div class="suscribete">
+                <form action="" class="vertical-middle flexbox">
+                    <label for="">suscribete</label>
+                    <input type="email" name="" id="" placeholder="E-mail">
+                    <button type="submit">
+                        <img src="<?php echo get_template_directory_uri() . '/img/arrow.png' ?>" alt="">
+                    </button>
+                </form>
+            </div>
+        </div>
+
+        <div class="row flexbox horizontal-center">
+            <div class="colmn" style="width: 40%">
+                <div class="card">
+                    <div class="overlay" style="background-image: url('<?php echo get_template_directory_uri() . '' ?>') "></div>
+                    <div class="leer-mas">
+                        <span>LEER MÁS</span>
+                        <span>
+                            <img src="" alt="">
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="colmn" style="width: 60%;">
+                <div class="row">
+                    <div class="card"></div>
+                    <div class="card"></div>
+                </div>
+                <div class="row">
+                    <div class="card"></div>
+                    <div class="card"></div>
+                    <div class="card"></div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- Blog -->
 
 <!-- testimonios -->
 <div class="row-container-fluid flexbox vertical-middle" style="background-image: url(<?php echo get_template_directory_uri() . '/img/bg-test.jpg'; ?>);">
